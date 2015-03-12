0ac234ea3d21b0038a996e7424d4bc99408ff804
alireza <alireza>
2015-03-12 13:26:52 +0330

    models

--- src/acution/commerce/models.py
+++ src/acution/commerce/models.py
@@ -1,1 +1,31 @@
+
 __author__ = 'naser'
+from django.db import models
+class User(models.Model):
+    fullname = models.CharField(max_length=100)
+    phone=models.CharField(max_length=20)
+    email = models.EmailField()
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=20)
+
+class Good(models.Model):
+    owner = models.ForeignKey(User)
+    owner_price = models.ForeignKey(Price)
+    city = models.CharField(max_length=20)
+    comments = models.TextField()
+    title = models.CharField(max_length=30)
+
+class Price(models.Model):
+    good = models.ForeignKey(Good)
+    user = models.ForeignKey(User)
+    amount = models.IntegerField()
+    date = models.DateTimeField()
+
+class Picture(models.Model):
+    good = models.ForeignKey(Good)
+    link = models.CharField(max_length=100)
+
+class Notification(models.Model):
+    good = models.ForeignKey(Good)
+    user = models.ForeignKey(User)
+    date_time = models.DateTimeField();

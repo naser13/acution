823dbbdb690d14fc46283fa4d52e5db69bf9c27f
naser13 <naser.aliabadi@gmail.com>
2015-03-13 15:09:37 +0330

    added label to form

--- src/acution/commerce/forms.py
+++ src/acution/commerce/forms.py
@@ -4,9 +4,14 @@ from acution.commerce.models import *
 
 class MemberForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
+
     class Meta:
         model = Member
         fields = ['first_name', 'last_name', 'username', 'password', 'email', 'phone']
+
+    def __init__(self):
+        super(MemberForm, self).__init__()
+        self.fields['username'].label = 'نام کاربری'
 
     def clean_first_name(self):
         first_name = self.cleaned_data['first_name']
@@ -16,13 +21,14 @@ class MemberForm(forms.ModelForm):
 
     def clean_last_name(self):
         last_name = self.cleaned_data['last_name']
-        if len(last_name)<1:
+        if len(last_name) < 1:
             raise forms.ValidationError('نمیتواند خالی باشد.')
         return last_name
 
 
 class LoginForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
+
     class Meta:
         model = Member
         fields = ['username', 'password']
@@ -35,24 +41,25 @@ class LoginForm(forms.ModelForm):
 
     def clean_password(self):
         last_name = self.cleaned_data['password']
-        if len(last_name)<1:
+        if len(last_name) < 1:
             raise forms.ValidationError('نمیتواند خالی باشد.')
         return last_name
+
 
 class AddGoodForm(forms.ModelForm):
     class Meta:
         model = Good
-        fields = ['title','comments','category']
+        fields = ['title', 'description', 'category']
 
     def clean_title(self):
         title = self.clean_data['title']
         if len(title) < 1:
-            raise forms.validationError("نمیتواند خالی باشد")
+            raise forms.ValidationError("نمیتواند خالی باشد")
         return title
 
     def clean_comments(self):
         comment = self.clean_data['comments']
-        if len(comment)<1 :
-            raise forms.validationError('نمیتواند خالی باشد')
+        if len(comment) < 1:
+            raise forms.ValidationError('نمیتواند خالی باشد')
         return comment
 
